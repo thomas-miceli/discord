7a910d1d1655bd46a492b3eae1bb1a645030e1f0
Thomas Miceli <tomus.mic@gmail.com>
2019-12-25 22:11:49 +0100

    Update command.js

--- actions/command.js
+++ actions/command.js
@@ -1,7 +1,6 @@
 class Command {
 	
 	static command (command, message) {
-		console.log(message.content.toLowerCase());
 		if (message.content.toLowerCase().startsWith(command.toLowerCase())) {
 
 			let args = message.content.split(' ');
